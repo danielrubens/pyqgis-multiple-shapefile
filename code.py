97b1ead00d4af331895f9286ccc68b4d66788d62
danielrubens <51061212+danielrubens@users.noreply.github.com>
2021-01-18 16:28:24 -0300

    Update code.py

--- code.py
+++ code.py
@@ -6,18 +6,18 @@ from qgis.gui import *
 import processing
 import sys
 
-BRAZIL = "C:\Users\daniel.rubens\Desktop\project\BR.shp"
+BRAZIL = "C:\Users\Desktop\project\BR.shp"
 iface.addVectorLayer(BRAZIL, "BRAZIL", "ogr")
 
 #Convertendo sistema de coordenadas
-GEO_SIRGAS = "C:\Users\daniel.rubens\Desktop\project/GEO_SIRGAS.shp"
+GEO_SIRGAS = "C:\Users\Desktop\project/GEO_SIRGAS.shp"
 processing.runalg("qgis:reprojectlayer", BRAZIL, "epsg:4674", GEO_SIRGAS)
 
 # Extraindo multiplos mapas de solo com um loop
 regioes =["NORTE", "NORDESTE", "SUL", "SUDESTE", "CENTRO-OESTE"]
 
 for n in regioes:
-  limite = "C:\Users\daniel.rubens\Desktop\project\lim" + n +".shp"
+  limite = "C:\Users\Desktop\project\lim" + n +".shp"
   processing.runalg('qgis:extractbyattribute', BRAZIL, "NM_REGIAO", 0, n, limite)
   iface.addVectorLayer(limite, "limite de " + n , "ogr")
 
